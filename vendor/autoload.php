b8022b5006b2472c53c1322463f77acba793b0a3
Ahmed Khan <codewithahmedkhan@gmail.com>
2025-07-20 12:35:12 +0400

    Establish third-party dependencies
    - Integrated PHPMailer for email functionality
    - Incorporated PHPUnit testing framework
    - Established Composer autoloading structure
    - Deployed external library dependencies

---
+++ vendor/autoload.php
@@ -0,0 +1,10 @@
+<?php
+// PHPMailer autoloader
+require_once __DIR__ . '/phpmailer/src/Exception.php';
+require_once __DIR__ . '/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . '/phpmailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+?>
